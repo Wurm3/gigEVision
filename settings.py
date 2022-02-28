278427320e64aaa4e1e2d9d5c9101d7e6a6d534d
unknown <=>
2022-02-28 14:57:15 +0100

    Added setting for configuration

---
+++ settings.py
@@ -0,0 +1,22 @@
+class Settings:
+
+    def __init__(self):
+        self.LED_PIN = 18
+        self.BLINK_FREQUENCY = 0.5
+
+        #flag if currently in picturemode
+        self.PICTURE_MODE = False
+
+        #if programm is running
+        self.running = True
+
+        #How many seconds to pause between images
+        self.IMAGE_PAUSE = 3
+
+        #path to visible images
+        self.VISIBLE_IMAGES_PATH = "images/visible_images/"
+
+        #path to infrared images
+        self.INFRARED_IMAGES_PATH = "images/thermal_images/"
+
+
