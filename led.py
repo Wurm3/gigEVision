fb96cb44ac637e17228a9468980e74074ddee71f
unknown <=>
2022-03-02 15:53:59 +0100

    Check if image thread is still alive

--- led.py
+++ led.py
@@ -1,10 +1,12 @@
 import RPi.GPIO as GPIO
 from threading import Thread
 import time
+from aquireimages import AquireImages
 
 class Led(Thread):
-    def __init__(self,settings):
+    def __init__(self, settings, aquire):
         Thread.__init__()
+        self.aquire = aquire
         self.settings = settings
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
@@ -13,8 +15,11 @@ class Led(Thread):
 
     def run(self):
         while self.settings.running:
-            GPIO.output(self.settings.LED_PIN, GPIO.HIGH)
-            time.sleep(self.settings.BLINK_FREQUENCY)
-            if not self.settings.PICTURE_MODE:
-                GPIO.output(self.settings.LED_PIN, GPIO.LOW)
+            if self.aquire.is_alive():
+                GPIO.output(self.settings.LED_PIN, GPIO.HIGH)
                 time.sleep(self.settings.BLINK_FREQUENCY)
+                if not self.settings.PICTURE_MODE:
+                    GPIO.output(self.settings.LED_PIN, GPIO.LOW)
+                    time.sleep(self.settings.BLINK_FREQUENCY)
+            else:
+                GPIO.output(self.settings.LED_PIN, GPIO.LOW)
