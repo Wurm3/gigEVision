606bd82d10f4d3f9527ef5fb47f9a92355144e38
unknown <=>
2022-03-02 16:39:31 +0100

    Added BUTTON_PIN variable

--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@ class Settings:
     def __init__(self):
         self.LED_PIN = 18
         self.BLINK_FREQUENCY = 0.5
+        self.BUTTON_PIN = 10
 
         #flag if currently in picturemode
         self.PICTURE_MODE = False
