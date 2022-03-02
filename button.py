762cfeb8b3ed7fd403ac8dfdf8b26ee5b5e60513
unknown <=>
2022-03-02 17:17:25 +0100

    Added long press option

--- button.py
+++ button.py
@@ -1,6 +1,8 @@
 import RPi.GPIO as GPIO
 from threading import Thread
 
+import time
+
 class Button(Thread):
 
     def __init__(self, settings):
@@ -8,13 +10,24 @@ class Button(Thread):
         Thread.__init__(self)
         #GPIO.setwarnings(False)
         #GPIO.setmode(GPIO.BOARD)
+        self.pressed = 0
         GPIO.setup(self.settings.BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
     def toggle_button(self):
+        print("toggle mode")
         self.settings.PICTURE_MODE = (False, True)[self.settings.PICTURE_MODE]
 
     def run(self):
         while self.settings.running:
-            print(GPIO.input(self.settings.BUTTON_PIN))
+            #print(GPIO.input(self.settings.BUTTON_PIN))
             if GPIO.input(self.settings.BUTTON_PIN) == GPIO.HIGH:
-                self.toggle_button()
+                if self.pressed == 0:
+                    self.pressed = time.time()
+            else:
+                if self.pressed != 0:
+                    duration = time.time() - self.pressed
+                    self.pressed = 0
+                    if duration > 2:
+                        self.settings.running = False
+                    else:
+                        self.toggle_button()
