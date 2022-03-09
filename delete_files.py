63f7a5bd484636e3adcbe88742aa572d8fb023bb
unknown <=>
2022-03-09 17:28:09 +0100

    changed os.unlink to os.remove

--- delete_files.py
+++ delete_files.py
@@ -8,7 +8,7 @@ def delete_folder_content(folder):
         file_path = os.path.join(folder, filename)
         try:
             if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
+                os.remove(file_path)
         except Exception as e:
             print('Failed to delete %s. Reason: %s' % (file_path, e))
 
