492e64ea0df05739f17f498d4974226cbb918466
unknown <=>
2022-03-14 11:56:25 +0100

    Corrected usage of wrong class

--- retrievedht11.py
+++ retrievedht11.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO
-from dht11.dht11 import DHT11Result
+from dht11.dht11 import DHT11
 
 class RetrieveDHT11:
     def __init__(self, settings):
@@ -10,7 +10,7 @@ class RetrieveDHT11:
 
     def get_data(self):
         result_map = {}
-        instance = DHT11Result(pin = self.settings.SENSOR_PIN)
+        instance = DHT11(pin = self.settings.SENSOR_PIN)
         result = innstance.read()
         if result.isValid():
             result_map["valid"] = True
