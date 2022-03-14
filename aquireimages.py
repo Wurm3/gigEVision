52dba578077d17a41502644ff63a47b8037b6c67
unknown <=>
2022-03-14 13:16:36 +0100

    Supplying tuple to string

--- aquireimages.py
+++ aquireimages.py
@@ -32,7 +32,7 @@ class AquireImages(threading.Thread):
         with open(self.settings.SENSOR_DATA_PATH, 'a+') as f:
             temp = map["temperature"]
             hum = map["humidity"]
-            str = "%s, %-3.1f, %-3.1f\n" % timestamp, temp, hum
+            str = "%s, %-3.1f, %-3.1f\n" % (timestamp, temp, hum)
             f.write(str)
 
     def run(self):
