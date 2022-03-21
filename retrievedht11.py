b4c4d30b13b85bae08ed56d89d1e74f46f86df46
unknown <=>
2022-03-21 13:48:27 +0100

    Added function to resend last value on one invalid retrieve

--- retrievedht11.py
+++ retrievedht11.py
@@ -4,6 +4,7 @@ from dht11.dht11 import DHT11
 class RetrieveDHT11:
     def __init__(self, settings):
         self.settings = settings
+        self.last_result = {}
 
     def get_data(self):
         result_map = {}
@@ -13,7 +14,12 @@ class RetrieveDHT11:
             result_map["valid"] = True
             result_map["temperature"] = result.temperature
             result_map["humidity"] = result.humidity
+            self.last_result = result_map
         else:
             result_map["valid"] = False
+            if self.last_result["valid"]:
+                tmp = result_map
+                result_map = self.last_result
+                self.last_result = tmp
 
         return result_map
