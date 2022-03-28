b7a5914ade31d1c99ad0227cc9266f251b1eba15
unknown <=>
2022-03-28 16:15:53 +0200

    Added new module for dht22 sensor

---
+++ dht22.py
@@ -0,0 +1,32 @@
+import time
+import board
+import adafruit_dht
+
+
+class DHT22:
+    def __init__(self, settings):
+        self.settings = settings
+        self.last_result = {"valid": False}
+
+    def get_data(self):
+        result_map = {}
+        instance = adafruit_dht.DHT22(self.settings.SENSOR_PIN, use_pulseio=False)
+        try:
+            result_map["valid"] = True
+            result_map["temperature"] = instance.temperature
+            result_map["humidity"] = instance.humidity
+            self.last_result = result_map
+        except RuntimeError as error:
+            result_map["valid"] = False
+            if self.last_result["valid"]:
+                tmp = result_map
+                result_map = self.last_result
+                self.last_result = tmp
+            time.sleep(0.1)
+        except Exception as error:
+            instance.exit()
+            raise error
+
+        return result_map
+
+
--- test.py
+++ test.py
@@ -3,50 +3,16 @@ from aquireimages import AquireImages
 from retrievedht11 import RetrieveDHT11
 from button import Button
 import RPi.GPIO as GPIO
-
 import time
-import board
-import adafruit_dht
 
-# Initial the dht device, with data pin connected to:
-# dhtDevice = adafruit_dht.DHT22(board.D4)
 
-# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
-# This may be necessary on a Linux single board computer like the Raspberry Pi,
-# but it will not work in CircuitPython.
-dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False)
-print(board.D4)
-
-while True:
-    try:
-        # Print the values to the serial port
-        temperature_c = dhtDevice.temperature
-        temperature_f = temperature_c * (9 / 5) + 32
-        humidity = dhtDevice.humidity
-        print(
-            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
-                temperature_f, temperature_c, humidity
-            )
-        )
-
-    except RuntimeError as error:
-        # Errors happen fairly often, DHT's are hard to read, just keep going
-        print(error.args[0])
-        time.sleep(2.0)
-        continue
-    except Exception as error:
-        dhtDevice.exit()
-        raise error
-
-    time.sleep(2.0)
-
-"""
 print("Starting....")
 settings = Settings()
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-"""
+
+
 """
 button = Button(settings)
 
@@ -57,7 +23,7 @@ settings.running = False
 button.join()
 
 """
-"""
+
 dht11 = RetrieveDHT11(settings)
 
 retrieve = True
@@ -77,7 +43,7 @@ while(retrieve):
     if counter >= 20:
         retrieve = False
 
-"""
+
 """
 #Start aquire thread
 aquire = AquireImages(settings)
