451a45d860f200d31d91f4b9ad66cd94315834fb
unknown <=>
2022-03-28 16:17:38 +0200

    Added humidity

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from retrievedht11 import RetrieveDHT11
 from button import Button
 import RPi.GPIO as GPIO
 import time
+import adafruit_dht
 
 
 print("Starting....")
@@ -42,6 +43,31 @@ while(retrieve):
     counter += 1
     if counter >= 20:
         retrieve = False
+
+dhtDevice = adafruit_dht.DHT22(settings.SENSOR_PIN, use_pulseio=False)
+
+while True:
+    try:
+        # Print the values to the serial port
+        temperature_c = dhtDevice.temperature
+        temperature_f = temperature_c * (9 / 5) + 32
+        humidity = dhtDevice.humidity
+        print(
+            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
+                temperature_f, temperature_c, humidity
+            )
+        )
+
+    except RuntimeError as error:
+        # Errors happen fairly often, DHT's are hard to read, just keep going
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        dhtDevice.exit()
+        raise error
+
+    time.sleep(2.0)
 
 
 """
